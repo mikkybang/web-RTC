95b363c62e2e2fe3aba65f768230534b729f701f
Michael Efemena <mikkybang@localhost.localdomain>
2020-06-12 22:38:27 +0100

    Add app.js and Index.html

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 async function playVideoFromCamera() {
-    try {
-        const constraints = {'video': true, 'audio': {'echoCancellation': true}};
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        const videoElement = document.querySelector('video#localVideo');
-        videoElement.srcObject = stream;
-    } catch(error) {
-        console.error('Error opening video camera.', error);
-    }
+  try {
+    //const constraints = { video: true, audio: { echoCancellation: true } };
+    //const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    const constraints = {
+      video: {
+        cursor: "always" | "motion" | "never",
+        displaySurface: "application" | "browser" | "monitor" | "window",
+      },
+    };
+    const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+    //console.log(MediaStreamTrack.getSettings())
+    const videoElement = document.querySelector("video#localVideo");
+    videoElement.srcObject = stream;
+  } catch (error) {
+    console.error("Error opening video camera.", error);
+  }
 }
 
-playVideoFromCamera()
+playVideoFromCamera();
