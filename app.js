aa8735e9aab697d47a7a8a164df3a74d016fd63f
Michael Efemena <mikkybang@localhost.localdomain>
2020-06-16 13:44:10 +0100

    move app.js and index.html to client folder

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 async function playVideoFromCamera() {
   try {
-    //const constraints = { video: true, audio: { echoCancellation: true } };
-    //const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    const constraints = {
-      video: {
-        cursor: "always" | "motion" | "never",
-        displaySurface: "application" | "browser" | "monitor" | "window",
-      },
-    };
-    const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
-    //console.log(MediaStreamTrack.getSettings())
+    const constraints = { video: true, audio: { echoCancellation: true } };
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    // const constraints = {
+    //   video: {
+    //     cursor: "always" | "motion" | "never",
+    //     displaySurface: "application" | "browser" | "monitor" | "window",
+    //   },
+    // };
+    // const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
     const videoElement = document.querySelector("video#localVideo");
     videoElement.srcObject = stream;
   } catch (error) {
