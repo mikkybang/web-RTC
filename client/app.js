dcc9f86010d98fa9706623551ee3ba1ba427d051
Michael Efemena <mikkybang@localhost.localdomain>
2020-06-19 16:55:58 +0100

    .

--- client/app.js
+++ client/app.js
@@ -148,3 +148,18 @@ peerConnection.ontrack = function({ streams: [stream] }) {
     remoteVideo.srcObject = stream;
   }
 };
+
+navigator.getUserMedia(
+  { video: true, audio: true },
+  stream => {
+    const localVideo = document.getElementById("local-video");
+    if (localVideo) {
+      localVideo.srcObject = stream;
+    }
+
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+  },
+  error => {
+    console.warn(error.message);
+  }
+);
