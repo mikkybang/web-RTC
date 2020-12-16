e78f0d613367a8f56d83f62e7f895eff5cf001d5
Michael Efemena <mefemena@gmail.com>
2020-12-17 00:28:36 +0100

    .

--- client/src/App.js
+++ client/src/App.js
@@ -4,65 +4,78 @@ import Peer from "simple-peer";
 import io from "socket.io-client";
 import "./App.css";
 
-const SOCKETIO = "localhost:9090";
+const SOCKETIO = "http://localhost:9090";
 
 function App() {
-  const [stream, setSteam] = useState();
-  const [yourId, setYourId] = useState("");
-  const [users, setUsers] = useState([]);
-  const [receivingCall, setRecevingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAccepted] = useState(false);
+  // const [stream, setSteam] = useState();
+  // const [yourId, setYourId] = useState("");
+  // const [users, setUsers] = useState([]);
+  // const [receivingCall, setRecevingCall] = useState(false);
+  // const [caller, setCaller] = useState("");
+  // const [callerSignal, setCallerSignal] = useState();
+  // const [callAccepted, setCallAccepted] = useState(false);
 
-  const userVideo = useRef();
-  const partnerVideo = useRef();
-  const socket = useRef();
+  // const userVideo = useRef();
+  // const partnerVideo = useRef();
+  let socket;
+  //const
 
   useEffect(() => {
-    socket.current = io(SOCKETIO);
-    // navigator.mediaDevices
-    //   .getUserMedia({ video: true, audio: true })
-    //   .then((stream) => {
-    //     setSteam(stream);
-    //     if (userVideo.current) {
-    //       userVideo.current.srcObject = stream;
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log("Could not get media stream");
-    //   });
-
-    socket.current.on("connect", (socket) => {
-      console.log("Client socket connected");
+    socket = io(SOCKETIO, { transports: ["websocket"] });
+    socket.on("clientCon", () => {
+      console.log("client connn");
     });
 
-    // console.log(socket.current);
+    socket.emit("clientHere", { name: "Michael" });
+  }, [SOCKETIO]);
 
-    // socket.current.on("yourId", (id) => {
-    //   setYourId(id);
-    // });
+  // useEffect(() => {
+  //   // navigator.mediaDevices
+  //   //   .getUserMedia({ video: true, audio: true })
+  //   //   .then((stream) => {
+  //   //     setSteam(stream);
+  //   //     if (userVideo.current) {
+  //   //       userVideo.current.srcObject = stream;
+  //   //     }
+  //   //   })
+  //   //   .catch((err) => {
+  //   //     console.log("Could not get media stream");
+  //   //   });
+  //   // console.log(socket);
+  //   // socket.on("connect", (socket) => {
+  //   //   console.log("Client socket connected");
+  //   // });
 
-    // socket.current.on("users", (data) => {
-    //   console.log(data.users);
-    //   setUsers(data.users);
-    // });
-  }, []);
+  //   // console.log(socket.current);
 
-  // let UserVideo;
-  // if (stream) {
-  //   UserVideo = <video playsInline muted ref={userVideo} autoPlay></video>;
-  // }
-  // let PartnerVideo;
-  // if (callAccepted) {
-  //   PartnerVideo = (
-  //     <video playsInline muted ref={partnerVideo} autoPlay></video>
-  //   );
-  // }
+  //   // socket.current.on("yourId", (id) => {
+  //   //   setYourId(id);
+  //   // });
 
-  const callPeer = (key) => {};
+  //   // socket.current.on("users", (data) => {
+  //   //   console.log(data.users);
+  //   //   setUsers(data.users);
+  //   // });
 
-  const acceptCall = () => {};
+  //   return () => {
+  //     socket.disconnect();
+  //   };
+  // }, [socket]);
+
+  // // let UserVideo;
+  // // if (stream) {
+  // //   UserVideo = <video playsInline muted ref={userVideo} autoPlay></video>;
+  // // }
+  // // let PartnerVideo;
+  // // if (callAccepted) {
+  // //   PartnerVideo = (
+  // //     <video playsInline muted ref={partnerVideo} autoPlay></video>
+  // //   );
+  // // }
+
+  // const callPeer = (key) => {};
+
+  // const acceptCall = () => {};
 
   return (
     <div className="App">
--- src/server.ts
+++ src/server.ts
@@ -2,68 +2,76 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import path from 'path'
 import { createServer, Server as HTTPServer } from 'http'
 import SocketIO, { Server as SocketIOServer } from "socket.io"
+import cors from "cors"
 
 const app: Application = express()
 const server: HTTPServer = createServer(app)
 const io: SocketIOServer = SocketIO(server)
+
+
+app.use(cors())
 
 var activeSockets: String[] = [];
 
 io.on("connection", (socket) => {
     console.log("Socket connected")
     console.log(socket.id)
-    const existingSocket = activeSockets.find(
-        existingSocket => existingSocket == socket.id
-    );
+    socket.emit("clientCon", null);
+    // const existingSocket = activeSockets.find(
+    //     existingSocket => existingSocket == socket.id
+    // );
 
-    if (!existingSocket) {
-        activeSockets.push(socket.id);
-        console.log("YourId", activeSockets)
-        socket.emit("yourId", socket.id);
+    // if (!existingSocket) {
+    //     activeSockets.push(socket.id);
+    //     console.log("YourId", activeSockets)
+    //     socket.emit("yourId", socket.id);
 
-        io.sockets.emit("users", {
-            users: activeSockets
-        });
-    }
+    //     io.sockets.emit("users", {
+    //         users: activeSockets
+    //     });
+    // }
+
+    socket.on("clientHere", data => {
+        console.log(data)
+    })
 
 
-    socket.on("call-user", data => {
-        io.to(data.to).emit("call-made", {
-            offer: data.offer,
-            socket: socket.id
-        });
-    });
+    // socket.on("call-user", data => {
+    //     io.to(data.to).emit("call-made", {
+    //         offer: data.offer,
+    //         socket: socket.id
+    //     });
+    // });
 
-    socket.on("make-answer", data => {
-        io.to(data.to).emit("answer-made", {
-            socket: socket.id,
-            answer: data.answer
-        });
-    });
+    // socket.on("make-answer", data => {
+    //     io.to(data.to).emit("answer-made", {
+    //         socket: socket.id,
+    //         answer: data.answer
+    //     });
+    // });
 
-    socket.on("reject-call", data => {
-        socket.to(data.from).emit("call-rejected", {
-            socket: socket.id
-        });
-    });
+    // socket.on("reject-call", data => {
+    //     socket.to(data.from).emit("call-rejected", {
+    //         socket: socket.id
+    //     });
+    // });
 
-    socket.on("disconnect", () => {
-        console.log("disconecting")
-        activeSockets = activeSockets.filter(
-            existingSocket => existingSocket !== socket.id
-        );
-        socket.broadcast.emit("remove-user", {
-            socketId: socket.id
-        });
-    });
+    // socket.on("disconnect", () => {
+    //     console.log("disconecting")
+    //     activeSockets = activeSockets.filter(
+    //         existingSocket => existingSocket !== socket.id
+    //     );
+    //     io.sockets.emit("users", {
+    //         users: activeSockets
+    //     });
+    // });
 
 
 
 })
 
+
 app.use(express.static(path.join(__dirname, "../legacy-client")));
-
-
 
 //Port
 app.set('port', process.env.PORT || 9090);
